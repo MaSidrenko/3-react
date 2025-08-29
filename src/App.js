149823d79ae6f0511192b40834566a24760f86fc
MaSidrenko <nexon863@gmail.com>
2025-08-30 02:14:35 +0300

    check changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This pages upload to github.com with gitbash
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/MaSidrenko"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          github.com/MaSidrenko
         </a>
       </header>
     </div>
