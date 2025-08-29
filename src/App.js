14a8a62cd56d5a26c4395e5e80462d196e40d36c
MaSidrenko <nexon863@gmail.com>
2025-08-30 02:18:08 +0300

    Upper Letter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This pages upload to github.com with gitbash
+          This pages upload to github.com with Git Bash
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          github.com/MaSidrenko
+          GitHub.com/MaSidrenko
         </a>
       </header>
     </div>
